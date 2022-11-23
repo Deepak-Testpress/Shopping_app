9e4754571c1c7510e2c1c496ec2b1554ef4eab8f
Deepak-Testpress <deepak@testpress.in>
2022-11-23 11:19:12 +0530

    Customize the admin look

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,9 @@ from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import send_order_creation_mail
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
 
 
 def order_create(request):
@@ -28,3 +31,9 @@ def order_create(request):
     return render(
         request, "orders/order/create.html", {"cart": cart, "form": form}
     )
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order/detail.html", {"order": order})
