2cd2caaa7db7775dafb102f362863ef77975bf86
Deepak-Testpress <deepak@testpress.in>
2022-11-29 16:21:53 +0530

    Add test cases for cart_remove method

--- myshop/tests/cart/test_views.py
+++ myshop/tests/cart/test_views.py
@@ -24,6 +24,13 @@ class TestViews(TestCase):
 
         self.assertEqual(cart_detail_url, response.url)
 
+    def test_cart_remove_redirects_to_cart_detail(self):
+        cart_remove_url = reverse("cart:cart_add", kwargs={"product_id": "1"})
+        cart_detail_url = reverse("cart:cart_detail")
+        response = self.client.post(cart_remove_url)
+
+        self.assertEqual(cart_detail_url, response.url)
+
     def test_templates_used_for_cart_detail_view(self):
         cart_detail_url = reverse("cart:cart_detail")
         response = self.client.get(cart_detail_url)
@@ -32,5 +39,10 @@ class TestViews(TestCase):
 
     def test_cart_add_returns_404_for_invalid_product(self):
         response = self.client.post(reverse("cart:cart_add", args=["2191"]))
+
+        self.assertEquals(response.status_code, 404)
+
+    def test_cart_remove_returns_404_for_invalid_product(self):
+        response = self.client.post(reverse("cart:cart_remove", args=["2191"]))
 
         self.assertEquals(response.status_code, 404)
