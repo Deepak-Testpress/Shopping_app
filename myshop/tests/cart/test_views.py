ea61b5c16e1988d7c25b138be7cf8d4a4d0081c6
Deepak-Testpress <110899393+Deepak-Testpress@users.noreply.github.com>
2022-12-02 18:29:46 +0530

    Support Adding an item to the cart (#7)
    * Create forms and views that support adding items to cart
    
    * Add test cases for views, forms and urls
    
    * Rename test cases
    
    * Add test cases for cart_view methods
    
    * Refactor testcases by minimizing instance variable

---
+++ myshop/tests/cart/test_views.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.urls import reverse
+from shop.models import Product, Category
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Gaming",
+            slug="gaming",
+        )
+        self.product = Product.objects.create(
+            category=self.category,
+            name="Play station 5",
+            slug="play-station-5",
+            description="A gaming console",
+            price=50000.00,
+        )
+
+    def test_cart_add_redirects_to_cart_detail(self):
+        cart_add_url = reverse("cart:cart_add", kwargs={"product_id": "1"})
+        cart_detail_url = reverse("cart:cart_detail")
+        response = self.client.post(cart_add_url)
+
+        self.assertEqual(cart_detail_url, response.url)
+
+    def test_templates_used_for_cart_detail_view(self):
+        cart_detail_url = reverse("cart:cart_detail")
+        response = self.client.get(cart_detail_url)
+
+        self.assertTemplateUsed(response, "cart/detail.html")
+
+    def test_cart_add_returns_404_for_invalid_product(self):
+        response = self.client.post(reverse("cart:cart_add", args=["2191"]))
+
+        self.assertEquals(response.status_code, 404)
